284bd982349cefef360075eed064857698c902f6
JaredShurtleff12 <jaredshurtleff12@gmail.com>
2022-10-19 16:56:00 -0400

    New files
    New files with survey 1 and 2 and a thank you page

---
+++ 1018GolfCourseProject/GolfCourseProject-main/GolfCourseProject/website/auth.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User, Review, Manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db 
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/survey', methods=['GET','POST'])
+def survey():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_Name = request.form.get('first_Name')
+        date = request.form.get('date')
+
+
+        new_user = User(email=email, first_Name=first_Name)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account created!', category='success')
+        return redirect(url_for('views.home'))
+
+    return render_template("user.html")
+
+@auth.route('/home')
+def home():
+    return render_template("home.html")
+
+@auth.route('/managementsignup', methods=['GET','POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_Name = request.form.get('first_Name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len('email') < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len('first_Name') < 2:
+            flash('Name must be longer than 1 character', category='error')
+        elif password1 != password2:
+            flash('Passwords must match, please check', category='error')
+        elif len('password1') < 7:
+            flash('Password must be greater than 7 characters', category='error')
+        else:
+            flash('Account created!', category='success')
+    return render_template("management.html")
+
+@auth.route('/managementlogin', methods=['GET','POST'])
+def login():
+    data = request.form
+    return render_template("managementLogin.html")
+
+
---
+++ 1018GolfCourseProject/GolfCourseProject/website/models.py
@@ -0,0 +1,28 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+# class Review(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     data = db.Column(db.String(10000))
+#     date = db.Column(db.DateTime(timezone=True), default=func.now())
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     q1 = db.Column(db.Integer)
+#     q2 = db.Column(db.Integer)
+#     q3 = db.Column(db.Integer)
+#     q4 = db.Column(db.Integer)
+#     q5 = db.Column(db.Integer)
+#     q6 = db.Column(db.Integer)
+#     q7 = db.Column(db.Integer)
+#     q8 = db.Column(db.Integer)
+#     q9 = db.Column(db.Integer)
+#     q10 = db.Column(db.Integer)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    first_Name = db.Column(db.String(150))
+    # last_name = db.Column(db.String(150))
+    # date = db.Column(db.DateTime(timezone=True), default=func.now())
+    # review = db.relationship('Review')
+
